65153f94c7fdb23c2f067c9c2a02a0d7ca64ad70
sarahduncan <sduncan@carbenefitsolutions.co.uk>
2025-03-12 16:56:29 +0000

    add error pages for dev and prod

---
+++ StrideStats.InnerApi/Controllers/ErrorHandlerController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StrideStats.InnerApi.Controllers
+{
+    [ApiExplorerSettings(IgnoreApi = true)]
+    [Route("[controller]")]
+    [ApiController]
+    [AllowAnonymous]
+    public class ErrorHandlerController : ControllerBase
+    {
+        [Route("error")]
+        public IActionResult Error()
+        {
+            return Problem();
+        }
+
+        [Route("error-development")]
+        public IActionResult HandleErrorDevelopment([FromServices] IHostEnvironment hostEnvironment)
+        {
+            if (!hostEnvironment.IsDevelopment())
+            {
+                return NotFound();
+            }
+
+            var exceptionHandlerFeature = HttpContext.Features.Get<IExceptionHandlerFeature>();
+
+            return Problem(detail: exceptionHandlerFeature.Error.InnerException.StackTrace, title: exceptionHandlerFeature.Error.Message);
+        }
+    }
+}
--- StrideStats.InnerApi/Program.cs
+++ StrideStats.InnerApi/Program.cs
@@ -20,9 +20,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseExceptionHandler("/errorhandler/error-development");
+}
+else
+{
+    app.UseExceptionHandler("/errorhandler/error");
 }
 
 app.UseHttpsRedirection();
