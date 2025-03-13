f117c29a0376cb84ec9f0bae1d4f2ea9e21cbc16
sarahduncan <sduncan@carbenefitsolutions.co.uk>
2025-03-13 12:23:39 +0000

    adds domain tests

---
+++ Domain.UnitTests/ApiRequests/GetAthleteApiRequestTests.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Requests;
+
+namespace Domain.UnitTests.ApiRequests
+{
+    public class GetAthleteApiRequestTests
+    {
+        [Fact]
+        public void GetUrl_ReturnsCorrectUrl()
+        {
+            var request = new GetAthleteApiRequest();
+
+            var url = request.GetUrl;
+
+            Assert.Equal("athlete", url);
+        }
+    }
+}
---
+++ Domain.UnitTests/ApiRequests/GetAthleteStatsApiRequestTests.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Requests;
+
+namespace Domain.UnitTests.ApiRequests
+{
+    public class GetAthleteStatsApiRequestTests
+    {
+        [Fact]
+        public void GetUrl_ReturnsCorrectUrl()
+        {
+            var athleteId = 123;
+            var request = new GetAthletesStatsApiRequest(athleteId);
+
+            var url = request.GetUrl;
+
+            Assert.Equal($"athletes/{athleteId}/stats", url);
+        }
+    }
+}
