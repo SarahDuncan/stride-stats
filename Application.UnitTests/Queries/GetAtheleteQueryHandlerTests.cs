311baa393bce6c176829071824027e7c096e1792
sarahduncan <sduncan@carbenefitsolutions.co.uk>
2025-03-13 11:44:18 +0000

    adds application layer unit tests

---
+++ Application.UnitTests/Queries/GetAtheleteQueryHandlerTests.cs
@@ -0,0 +1,56 @@
+﻿using Application.Queries.GetAthlete;
+using AutoMapper;
+using Domain.Interfaces.Api;
+using Domain.Requests;
+using Domain.Responses;
+using Moq;
+
+namespace Application.UnitTests.Queries
+{
+    public class GetAtheleteQueryHandlerTests
+    {
+        private readonly Mock<IApiClient> _mockApiClient;
+        private readonly Mock<IMapper> _mockMapper;
+        private readonly GetAthleteQueryHandler getAthleteQueryHandler;
+
+        public GetAtheleteQueryHandlerTests()
+        {
+            _mockApiClient = new Mock<IApiClient>();
+            _mockMapper = new Mock<IMapper>();
+            getAthleteQueryHandler = new GetAthleteQueryHandler(_mockApiClient.Object, _mockMapper.Object);
+        }
+
+        [Fact]
+        public async Task Handle_ReturnsGetAthleteQueryResult()
+        {
+            var getAthleteApiResponse = new GetAthleteApiResponse();
+            var getAthleteQueryResult = new GetAthleteQueryResult();
+            _mockApiClient.Setup(x => x.Get<GetAthleteApiResponse>(It.IsAny<GetAthleteApiRequest>())).ReturnsAsync(getAthleteApiResponse);
+            _mockMapper.Setup(x => x.Map<GetAthleteQueryResult>(getAthleteApiResponse)).Returns(getAthleteQueryResult);
+
+            var result = await getAthleteQueryHandler.Handle(new GetAthleteQuery(), CancellationToken.None);
+
+            Assert.Equal(result, getAthleteQueryResult);
+        }
+
+        [Fact]
+        public async Task Handle_ThrowsException_WhenApiCallFails()
+        {
+            _mockApiClient.Setup(api => api.Get<GetAthleteApiResponse>(It.IsAny<GetAthleteApiRequest>())).ThrowsAsync(new Exception("Api call failed"));
+            var query = new GetAthleteQuery();
+
+            await Assert.ThrowsAsync<Exception>(() => getAthleteQueryHandler.Handle(query, CancellationToken.None));
+        }
+
+        [Fact]
+        public async Task Handle_ThrowsException_WhenMappingFails()
+        {
+            var getAthleteApiResponse = new GetAthleteApiResponse();
+            _mockApiClient.Setup(x => x.Get<GetAthleteApiResponse>(It.IsAny<GetAthleteApiRequest>())).ReturnsAsync(getAthleteApiResponse);
+            _mockMapper.Setup(x => x.Map<GetAthleteQueryResult>(getAthleteApiResponse)).Throws(new Exception("Mapping failed"));
+            var query = new GetAthleteQuery();
+
+            await Assert.ThrowsAsync<Exception>(() => getAthleteQueryHandler.Handle(query, CancellationToken.None));
+        }
+    }
+}
---
+++ Application.UnitTests/Queries/GetAthletesStatsQueryHandlerTests.cs
@@ -0,0 +1,56 @@
+﻿using Application.Queries.GetAthletesStats;
+using AutoMapper;
+using Domain.Interfaces.Api;
+using Domain.Requests;
+using Domain.Responses;
+using Moq;
+
+namespace Application.UnitTests.Queries
+{
+    public class GetAthletesStatsQueryHandlerTests
+    {
+        private readonly Mock<IApiClient> _mockApiClient;
+        private readonly Mock<IMapper> _mockMapper;
+        private readonly GetAthletesStatsQueryHandler getAthletesStatsQueryHandler;
+
+        public GetAthletesStatsQueryHandlerTests()
+        {
+            _mockApiClient = new Mock<IApiClient>();
+            _mockMapper = new Mock<IMapper>();
+            getAthletesStatsQueryHandler = new GetAthletesStatsQueryHandler(_mockApiClient.Object, _mockMapper.Object);
+        }
+
+        [Fact]
+        public async Task Handle_ReturnsGetAthletesStatsQueryResult()
+        {
+            var getAthletesStatsApiResponse = new GetAthletesStatsApiResponse();
+            var getAthletesStatsQueryResult = new GetAthletesStatsQueryResult();
+            _mockApiClient.Setup(x => x.Get<GetAthletesStatsApiResponse>(It.IsAny<GetAthletesStatsApiRequest>())).ReturnsAsync(getAthletesStatsApiResponse);
+            _mockMapper.Setup(x => x.Map<GetAthletesStatsQueryResult>(getAthletesStatsApiResponse)).Returns(getAthletesStatsQueryResult);
+
+            var result = await getAthletesStatsQueryHandler.Handle(new GetAthletesStatsQuery(), CancellationToken.None);
+
+            Assert.Equal(result, getAthletesStatsQueryResult);
+        }
+
+        [Fact]
+        public async Task Handle_ThrowsException_WhenApiCallFails()
+        {
+            _mockApiClient.Setup(api => api.Get<GetAthletesStatsApiResponse>(It.IsAny<GetAthletesStatsApiRequest>())).ThrowsAsync(new Exception("Api call failed"));
+            var query = new GetAthletesStatsQuery();
+
+            await Assert.ThrowsAsync<Exception>(() => getAthletesStatsQueryHandler.Handle(query, CancellationToken.None));
+        }
+
+        [Fact]
+        public async Task Handle_ThrowsException_WhenMappingFails()
+        {
+            var getAthletesStatsApiResponse = new GetAthletesStatsApiResponse();
+            _mockApiClient.Setup(x => x.Get<GetAthletesStatsApiResponse>(It.IsAny<GetAthletesStatsApiRequest>())).ReturnsAsync(getAthletesStatsApiResponse);
+            _mockMapper.Setup(x => x.Map<GetAthletesStatsQueryResult>(getAthletesStatsApiResponse)).Throws(new Exception("Mapping failed"));
+            var query = new GetAthletesStatsQuery();
+
+            await Assert.ThrowsAsync<Exception>(() => getAthletesStatsQueryHandler.Handle(query, CancellationToken.None));
+        }
+    }
+}
