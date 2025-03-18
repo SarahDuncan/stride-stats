99ab398249aff053d5d4f6e67ecadd76076b859c
sarahduncan <sduncan@carbenefitsolutions.co.uk>
2025-03-18 11:34:53 +0000

    updates integration tests to use wiremock

--- IntegrationTests/Tests/Controllers/AthleteControllerTests.cs
+++ IntegrationTests/Tests/Controllers/AthleteControllerTests.cs
@@ -1,22 +1,66 @@
 ﻿using Domain.Responses;
 using Microsoft.AspNetCore.Mvc.Testing;
 using System.Text.Json;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
 
 namespace IntegrationTests.Tests.Controllers
 {
     public class AthleteControllerTests : IClassFixture<WebApplicationFactory<Program>>
     {
         private readonly HttpClient _httpClient;
+        private readonly WebApplicationFactory<Program> _factory;
+        private readonly WireMockServer _wireMockServer;
+        private readonly JsonSerializerOptions jsonOptions;
 
-        public AthleteControllerTests(WebApplicationFactory<Program> webApplicationFactory)
+        public AthleteControllerTests(WebApplicationFactory<Program> factory)
         {
-            _httpClient = webApplicationFactory.CreateDefaultClient(new Uri("https://localhost/api/athlete"));
+            _wireMockServer = WireMockServer.Start();
+            Console.WriteLine($"WireMock server started at {_wireMockServer.Urls[0]}");
+
+            _factory = factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureAppConfiguration((context, config) =>
+                {
+                    config.AddInMemoryCollection(new Dictionary<string, string>()
+            {
+                {"ApiSettings:BaseUrl", _wireMockServer.Urls[0] },
+                {"ApiSettings:AccessToken", "mock_token" }
+            });
+                });
+
+                // Add this to verify the configuration is correctly applied
+                builder.ConfigureServices(services =>
+                {
+                    services.AddLogging(logging =>
+                    {
+                        logging.AddConsole();
+                        logging.SetMinimumLevel(LogLevel.Debug);
+                    });
+                });
+            });
+
+            _httpClient = _factory.CreateClient();
+            jsonOptions = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
         }
 
         [Fact]
         public async Task GetAthlete_ReturnsSuccessStatusCode()
         {
-            var response = await _httpClient.GetAsync("");
+            var expectedResponseJson = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "MockResponses", "mockAthleteData.json"));
+            _wireMockServer.Given(Request.Create()
+                .WithPath("/athlete")
+                .UsingGet())
+                .RespondWith(Response.Create()
+                .WithStatusCode(200)
+                .WithHeader("Content-Type", "application/json")
+                .WithBody(expectedResponseJson));
+
+            var response = await _httpClient.GetAsync("/api/athlete");
 
             response.EnsureSuccessStatusCode();
         }
@@ -24,13 +68,18 @@ namespace IntegrationTests.Tests.Controllers
         [Fact]
         public async Task GetAthlete_ReturnsExpectedResponse()
         {
-            var expectedResponse = JsonSerializer.Deserialize<GetAthleteApiResponse>
-                (File.ReadAllText("MockResponses/GetAthleteApiResponse.json"));
-
-            var model = await _httpClient.GetFromJsonAsync<GetAthleteApiResponse>("");
-
-            Assert.Equal(expectedResponse, model);
-            Assert.NotNull(model?.FirstName);
+            var expectedResponseJson = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "MockResponses", "mockAthleteData.json"));
+            var expectedResponse = JsonSerializer.Deserialize<GetAthleteApiResponse>(expectedResponseJson, jsonOptions);
+            _wireMockServer.Given(Request.Create().WithPath("/athlete").UsingGet())
+                .RespondWith(Response.Create()
+                    .WithStatusCode(200)
+                    .WithHeader("Content-Type", "application/json")
+                    .WithBody(expectedResponseJson));
+            var response = await _httpClient.GetAsync("/api/athlete");
+            var content = await response.Content.ReadAsStringAsync();
+            var model = await response.Content.ReadFromJsonAsync<GetAthleteApiResponse>(jsonOptions);
+            Assert.NotNull(model);
+            Assert.Equal(expectedResponse?.FirstName, model.FirstName);
         }
     }
 }
